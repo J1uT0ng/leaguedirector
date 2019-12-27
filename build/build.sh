ba05e66fb13fbac54042674294f12736d23cea45
J1uT0ng <je1dispen@gmail.com>
2019-12-27 18:06:41 +0800

    Delete build.sh

--- build/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd "${0%/*}"
-pip install pipenv
-pipenv install --skip-lock
-pipenv run pip install pyinstaller==3.4
-pipenv run pyinstaller build.spec --windowed --noconfirm --workpath=out/build --distpath=out/dist
